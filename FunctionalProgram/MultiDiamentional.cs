5240947963a1c37c55eb4aaa1605842ed6079efa
farhinsheikh <farhinsheikh2015@gmail.com>
2021-03-31 16:08:01 +0530

    add functional program file

---
+++ FunctionalProgram/MultiDiamentional.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgram
+{
+    class MultiDiamentional
+    {
+        public static void ReadMultiArray()
+        {
+            object[][] array = new object[4][];
+            array[0] = new object
+        }
+    }
+}
---
+++ FunctionalProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
